2507fbfe5090e9af74e869367b504a52d96e2609
Kaylie Au <60458584+krucian@users.noreply.github.com>
2020-04-06 23:24:14 -0700

    Add files via upload

---
+++ 339_Portfolio/Portfolio/js/main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "nav_bar") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav_bar";
+  }
+}
